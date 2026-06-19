6db2b3a6e52c04deddcf2d2954e02df9b638bc2c
Tushar Verma <tusharverma.94512@gmail.com>
2026-06-19 16:21:04 +0530

    Mesaage

--- Day 10/blockking_problem.py
+++ Day 10/blockking_problem.py
@@ -6,10 +6,10 @@ def fake_api_call(name,seconds):
     print(f"done {name}")
     return f"{name}result"
 
-start = time.pref_counter()
+start = time.perf_counter()
 r1= fake_api_call("model-A",1.0)
 r2= fake_api_call("model-B",1.0)
 r3= fake_api_call("model-C",1.0)
-elapsed = time.pref_counter() - start
+elapsed = time.perf_counter() - start
 print("Results",[r1,r2,r3])
 print(f"Three 1-second calls took{elapsed}")
