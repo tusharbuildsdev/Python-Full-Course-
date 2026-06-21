eef11b1636e27cde44a59443c881c969804b58e2
Tushar Verma <tusharverma.94512@gmail.com>
2026-06-21 21:35:52 +0530

    Completed Day 05

--- Day 05/args_kwargs.py
+++ Day 05/args_kwargs.py
@@ -7,7 +7,7 @@ def total(*args):
     print(" args is a tuple", args)
     return sum(args)
 
-print("Total (10,20,40) =", total(10,20,40))
+print("Total (15,20,40) =", total(15,20,40))
 print("Total (10,250) =", total(10,250))
 print("Total () =", total())
 
@@ -17,5 +17,5 @@ def make_user(**kwargs):
     print(" kwargs is a dict", kwargs)
     return kwargs
 
-make_user(name="Arjit", age=34,city="Lucknow")
+make_user(name="Tushar", age=34,city="Lucknow")
 make_user(name="Ben")
