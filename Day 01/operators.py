84d1738fed51a0fa8390ac653fa88eae9c6b57dc
Tushar Verma <tusharverma.94512@gmail.com>
2026-07-12 22:57:03 +0530

    Initial Commit

--- Day 01/operators.py
+++ Day 01/operators.py
@@ -1,5 +1,5 @@
 # ----- The familiar four -----
-print("3 + 2 =", 3 + 2)
+print("3 + 2 =", 3 + 9)
 print("3 - 2 =", 3 - 2)
 print("3 * 2 =", 3 * 2)
 print("7 / 2 =", 7 / 2)     # 3.5  (true division -> float)
