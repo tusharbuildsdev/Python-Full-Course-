65ead17ef0e09d3b5f7ac753992d1c383db7883d
Tushar Verma <tusharverma.94512@gmail.com>
2026-07-12 22:55:44 +0530

    Initial Commit

--- Day 01/numbers.py
+++ Day 01/numbers.py
@@ -6,7 +6,7 @@ print("Type of 42 ->", type(42))
 print("Float Numners", 3.14)
 print("Type of 3.14", type(3.14))
 
-print("Type of 4/2", type(4/2))
+print("Type of 6/2", type(6/2))
 
 print(0.1 + 0.2)
 print(2 ** 100)
